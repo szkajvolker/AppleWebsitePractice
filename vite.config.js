9d87fb8e0a7aa6218cf1b0b7340f08261df89f62
szkajvolker <ist9595@windowslive.com>
2025-08-22 16:54:51 +0200

    *

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-glw",
-    project: "javascript-react"
-  })],
+  base: "/AppleWebsitePractice/",
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-glw",
+      project: "javascript-react",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
